3f2ea781372ce2244d52d3f5ffe2c0e37b7d1cd7
Cherkesky <guy@cherkesky.com>
2020-01-14 12:05:27 -0600

    Chicken Monkeyyyyyyyyyyyyyyy

--- chickenmonkey.py
+++ chickenmonkey.py
@@ -7,3 +7,12 @@ For numbers which are multiples of both five and seven print "ChickenMonkey".
 To determine if a number can be evenly divided by 5 or 7, use the Python modulo operator.
 '''
 
+nums_from_1_to_100 = range(1,101,1)
+for num in nums_from_1_to_100:
+  if num%5 == 0:
+    if num%7 ==0:
+          print(f'{num}: Chicken Monkey!!!')
+    print(f'{num}: Chicken')
+  elif num%7 == 0:
+      print(f'{num}: Monkey')
+  else: print(num)
